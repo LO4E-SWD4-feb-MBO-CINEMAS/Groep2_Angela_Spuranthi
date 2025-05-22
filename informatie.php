7a68e2f11838955fa8c27c4f5a0d626a51e9c609
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-05-22 12:13:58 +0200

    paginas aangemaakt

---
+++ informatie.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informatie MBO CINEMA'S</title>
+    <meta name="description" content="Blijf op de hoogte van de nieuwste films, trailers, recensies en bioscoopnieuws. Alles over films op één plek.">
+    <meta name="author" content="Spuranthi Srirangam">
+    <link rel="stylesheet" href="CSS/Style.css">
+</head>
+<body>
+    
+</body>
+</html>
