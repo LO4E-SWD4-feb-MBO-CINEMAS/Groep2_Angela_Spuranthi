828ffe5b504eca616afcd0ab587014c966a961b4
Angela Bansie <6044767@mborijnland.nl>
2025-05-22 12:09:23 +0200

    aanpassingen header/footer

--- Index.php
+++ Index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Ontdek de nieuwste films & bioscopen</title>
+    <title>Ontdek de nieuwste films & bioscopen</title>
     <meta name="description" content="Blijf op de hoogte van de nieuwste films, trailers, recensies en bioscoopnieuws. Alles over films op één plek.">
     <meta name="author" content="Angela Bansie">
-    <link rel="stylesheet" href="Groep2_Angela_Spuranthi\CSS\Style.css">
+    <link rel="stylesheet" href="\Groep2_Angela_Spuranthi\CSS\Style.css">
 </head>
 <body>
+  
+    <?php include 'header.php'; ?>
+    <?php include 'footer.php'; ?>
 
-    
 </body>
 </html>
