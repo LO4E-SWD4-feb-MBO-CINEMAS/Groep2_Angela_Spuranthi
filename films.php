31798c71d3a6bf40b9ac009d8c134bafef132152
Angela Bansie <6044767@mborijnland.nl>
2025-05-26 09:40:33 +0200

    films aanpassigen

---
+++ films.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ontdek de nieuwste films & bioscopen</title>
+    <meta name="description" content="Blijf op de hoogte van de nieuwste films, trailers, recensies en bioscoopnieuws. Alles over films op één plek.">
+    <meta name="author" content="Angela Bansie">
+    <link rel="stylesheet" href="\Groep2_Angela_Spuranthi\CSS\Style.css">
+</head>
+<body>
+  
+    <?php include 'header.php'; ?>
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
