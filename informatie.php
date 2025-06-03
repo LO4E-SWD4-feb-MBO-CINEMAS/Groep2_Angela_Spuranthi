4e3417c0ff057b5ce1d164857f72e8dd08e0e3ff
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-06-03 19:23:46 +0200

    informatie pagina
    Bijna klaar met de pagina maar ik vind het nog een beetje droog eruit zien

--- informatie.php
+++ informatie.php
@@ -7,6 +7,7 @@
     <meta name="description" content="Blijf op de hoogte van de nieuwste films, trailers, recensies en bioscoopnieuws. Alles over films op één plek.">
     <meta name="author" content="Spuranthi Srirangam">
     <link rel="stylesheet" href="CSS/Style.css">
+    <script src="JS/social_buttons.js" defer></script>
 </head>
 <body>
     <header>
@@ -20,13 +21,40 @@
 
     <section class="info_1">
         <article class="contactgegevens">
+            <h1>Wilt u contact met ons opnemen?</h1>
+            <br><p>Dan kunt u ons e-mailen via <b>info@MBOcinemas.nl</b> of bellen via <b>+31 6 2373282.</b></p>
+            <br>
+            <h1>Beleef de beste films bij MBOcinemas</h1>
+            <p>Bij MBOcinemas geniet je van de <b>nieuwste en beste</b> films in een <br>comfortabele en moderne omgeving.<br> Of je nu houdt van actie, romantiek, komedie of een<br> meeslepende documentaire —bij ons is er voor ieder wat wils. <br>Dankzij onze scherpe beeldkwaliteit en kraakhelder geluid <br>voelt elke film als een première!</p>
+            <h1></h1>
         </article>
         <article class="misc">
             <article class="openingstijden">
+                <article class="social-buttons">
+                    <button>
+                        <i class="bx bxl-instagram-alt"></i>
+                        <article class="bg"></article>
+                    </button>
+                    <button>
+                        <i class="bx bxl-youtube"></i>
+                        <article class="bg"></article>
+                    </button>
+                    <button>
+                        <i class="bx bxl-twitter"></i>
+                        <article class="bg"></article>
+                    </button>
+                    <button>
+                        <i class="bx bxl-linkedin"></i>
+                        <article class="bg"></article>
+                    </button>
+                </article>
             </article>
-            
+
             <article class="extra_info">
-                <h1>Openingstijden:</h1>
+                <h1>Openingstijden:</h1><br>
+                <p> Wij zijn elke maandag t/m donderdag open tot 23:00.</p>
+                <p> Vrijdag t/m zondag zijn wij open tot 01:00. </p><br>
+                <p> (Tijden kunnen variëren op feestdagen)</p>
             </article>
 
         </article>
