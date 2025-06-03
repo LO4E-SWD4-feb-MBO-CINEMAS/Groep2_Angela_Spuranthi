0c3bf47778b8cd62682527d994c058a074801b42
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-06-03 23:36:22 +0200

    social media knoppen werken

--- informatie.php
+++ informatie.php
@@ -22,31 +22,43 @@
     <section class="info_1">
         <article class="contactgegevens">
             <h1>Wilt u contact met ons opnemen?</h1>
-            <br><p>Dan kunt u ons e-mailen via <b>info@MBOcinemas.nl</b> of bellen via <b>+31 6 2373282.</b></p>
+            <br><p>Dan kunt u ons e-mailen via <b>info@MBOcinema.nl</b> of bellen via <b>+31 6 2373282.</b></p>
             <br>
-            <h1>Beleef de beste films bij MBOcinemas</h1>
-            <p>Bij MBOcinemas geniet je van de <b>nieuwste en beste</b> films in een <br>comfortabele en moderne omgeving.<br> Of je nu houdt van actie, romantiek, komedie of een<br> meeslepende documentaire —bij ons is er voor ieder wat wils. <br>Dankzij onze scherpe beeldkwaliteit en kraakhelder geluid <br>voelt elke film als een première!</p>
-            <h1></h1>
+            <h1>Beleef de beste films bij MBOcinema</h1>
+            <p>Bij MBOcinema geniet je van de <b>nieuwste en beste</b> films in een <br>comfortabele en moderne omgeving.<br> Of je nu houdt van actie, romantiek, komedie of een<br> meeslepende documentaire —bij ons is er voor ieder wat wils. <br>Dankzij onze scherpe beeldkwaliteit en kraakhelder geluid <br>voelt elke film als een première!</p>
+            <p>MBOcinema werd opgericht in 2010 met één doel: <br>film weer een echte beleving maken. <br>Wat begon als een kleine buurtbioscoop groeide al snel uit tot een<br> geliefde plek voor filmliefhebbers uit de hele regio.<br> Met een passie voor cinema en een hart voor <br>gastvrijheid blijven we ons ontwikkelen<br> — altijd met het publiek op de eerste rij.</p>
         </article>
         <article class="misc">
             <article class="openingstijden">
                 <article class="social-buttons">
-                    <button>
-                        <i class="bx bxl-instagram-alt"></i>
-                        <article class="bg"></article>
-                    </button>
-                    <button>
-                        <i class="bx bxl-youtube"></i>
-                        <article class="bg"></article>
-                    </button>
-                    <button>
-                        <i class="bx bxl-twitter"></i>
-                        <article class="bg"></article>
-                    </button>
-                    <button>
-                        <i class="bx bxl-linkedin"></i>
-                        <article class="bg"></article>
-                    </button>
+                    <a href="https://www.instagram.com/spuranthi/" target="_blank" rel="noopener noreferrer">
+                        <button>
+                            <i class="bx bxl-instagram-alt"></i>
+                            <article class="bg"></article>
+                        </button>
+                    </a>
+
+                    <a href="https://www.youtube.com/watch?v=Eo9-g_EGaSk&list=LL&index=3&ab_channel=mboRijnland" target="_blank" rel="noopener noreferrer">
+                        <button>
+                            <i class="bx bxl-youtube"></i>
+                            <article class="bg"></article>
+                        </button>
+                    </a>
+
+                    <a href="https://x.com/heartstr0ke1" target="_blank" rel="noopener noreferrer">
+                        <button>
+                            <i class="bx bxl-twitter"></i>
+                            <article class="bg"></article>
+                        </button>
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/spuranthi-srirangam-106742309/" target="_blank" rel="noopener noreferrer">
+                        <button>
+                            <i class="bx bxl-linkedin"></i>
+                            <article class="bg"></article>
+                        </button>
+                    </a>
+
                 </article>
             </article>
 
